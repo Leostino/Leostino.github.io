12002a72e105aca06ceee488f62ad610a443136b
Leonard Ezeudu <leonardezeudu@gmail.com>
2019-11-06 16:43:39 -0500

    Updates

---
+++ precache-manifest.d9d47074482b6eeb7258e5c948b3a45a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92b0de4821c669da0aa0cdf19ecaa60",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "3ff9ddf266885f490d2e",
+    "url": "/ClickyGame/static/css/main.4ef257d1.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/ClickyGame/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "3ff9ddf266885f490d2e",
+    "url": "/ClickyGame/static/js/main.44eeec90.chunk.js"
+  },
+  {
+    "revision": "8ba95286b7700d634216",
+    "url": "/ClickyGame/static/js/runtime-main.8737585c.js"
+  },
+  {
+    "revision": "216f1847f3d480923e09d4bf46770e5f",
+    "url": "/ClickyGame/static/media/background-img.216f1847.jpg"
+  }
+]);
